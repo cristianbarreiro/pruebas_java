11f636a3a2e885d96a49453cadd762feb0615f49
Cristian Barreiro <122134947+cristianbarreiro@users.noreply.github.com>
2026-08-25 02:32:18 -0300

    feat: pt1 spring cloud netflix eureka

---
+++ OnlineStore/src/main/java/com/example/model/Device.java
@@ -0,0 +1,21 @@
+package com.example.model;
+
+public class Device {
+    String name;
+    String type;
+
+    public Device(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public Device() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
---
+++ OnlineStore/src/main/java/com/example/model/Devices.java
@@ -0,0 +1,17 @@
+package com.example.model;
+
+import java.util.List;
+
+public class Devices {
+    String brand;
+    List<Device> devices;
+
+    public Devices() {
+
+    }
+
+    public Devices(String brand, List<Device> devices) {
+        this.brand = brand;
+        this.devices = devices;
+    }
+}
