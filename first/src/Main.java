3f5d98183b50fb6341758562b4490869285174d8
Cristian Barreiro <122134947+cristianbarreiro@users.noreply.github.com>
2026-09-04 02:20:26 -0300

    feat: stream operations in java

--- first/src/Main.java
+++ first/src/Main.java
@@ -1,5 +1,8 @@
 //interfaces de java
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.*;
+import java.util.stream.Collectors;
 
 @FunctionalInterface
 interface Greeting {
@@ -24,8 +27,24 @@ public class Main {
         // Predicate<Integer> isEven = number -> number % 2 == 0;
         // System.out.println(isEven.test(4));
 
-        Function<Integer, Integer> square = number -> number * number;
-        System.out.println(square.apply(10));
+        // Function<Integer, Integer> square = number -> number * number;
+        // System.out.println(square.apply(10));
 
+        // Stream Operations
+        List<Integer> numbers = Arrays.asList(10, 30, 25, 45, 50);
+        System.out.println(numbers);
+
+        // numbers.stream().filter(n -> n > 30).forEach(System.out::println);
+
+        // Utilizando map
+        // numbers.stream().map(n -> n * n).forEach(System.out::println);
+
+        // numbers.stream().sorted().forEach( n -> System.out.println(n));
+
+        // numbers.stream().filter(n -> n > 20).map(n -> n * n).forEach(System.out::println);
+
+        List<Integer> result = numbers.stream().filter(n -> n > 20).collect(Collectors.toList());
+
+        System.out.println(result);
     }
 }
