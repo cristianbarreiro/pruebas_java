8f55231c00c75eaecc10b76a5fda9e8b041ed06f
Cristian Barreiro <122134947+cristianbarreiro@users.noreply.github.com>
2026-09-05 17:43:04 -0300

    feat: jdbc connection

---
+++ jdbcConnection/src/Main.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/demo";
+        String username = "root";
+        String password = "Yash11";
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected to database successfully!");
+        } catch (SQLException e) {
+            System.out.println("Connection failed!");
+            e.printStackTrace();
+        }
+
+    }
+}
